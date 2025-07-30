7b7f28649d889e89cea5f19435d62a7bd9424bec
AniketBahukhandi <aniketbahukhandi@gmail.com>
2025-07-30 13:55:34 +0530

    new fixes

--- backend/index.mjs
+++ backend/index.mjs
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin: "http://localhost:5173",
+    origin: "https://devgluck.vercel.app/",
     credentials: true,
   }
 ));
--- backend/src/socket/socket.mjs
+++ backend/src/socket/socket.mjs
@@ -9,7 +9,7 @@ export let io;
 export const initSocket = (server) => {
     io = new Server(server, {
         cors: {
-            origin: "http://localhost:5173",
+            origin: "https://devgluck.vercel.app/",
             methods: ["GET", "POST"],
             credentials: true,
         },
